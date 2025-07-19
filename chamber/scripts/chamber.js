fc823cc2c2713c53801f3b446a48993ed3d60203
BoTBry <abchigbu@gmail.com>
2025-07-19 17:53:15 +0100

    get link

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,12 +1,35 @@
 const info_section = document.querySelector(".info_section");
 const current_year = document.querySelector(".current_year");
 const lastModification = document.querySelector(".lastModification");
+const myNameElement = document.querySelector(".name");
+const myName = "Chigbu Chiemela Bryan";
 
 const get_year = new Date();
 const last_modified_date = document.lastModified;
 
 current_year.textContent = `${get_year.getFullYear()}`;
 lastModification.textContent = `${last_modified_date}`;
+myNameElement.textContent = `${myName}`
+
+// Notification Toggle
+const menu = document.querySelector("#menu");
+const nav = document.querySelector("nav");
+const header = document.querySelector("header");
+const mode = document.querySelector(".mode");
+const light_dark_mode = document.querySelector("body");
+const logo = document.querySelector(".logo");
+
+menu.addEventListener("click", () => {
+    nav.classList.toggle("show");
+    header.classList.toggle("close");
+
+})
+
+mode.addEventListener("click", () => {
+    mode.classList.toggle("switch");
+    light_dark_mode.classList.toggle("light_background");
+    light_dark_mode.classList.toggle("dark_background")
+})
 
 // Testing element:
 // info_section.innerHTML = "Hello world";
@@ -30,6 +53,7 @@ const create_section = (array) => {
     array.forEach(element => {
         // Creates elements need for the info card
         let card = document.createElement("section");
+        let info_div = document.createElement("div");
         let card_header = document.createElement("h2");
         let card_email_para = document.createElement("p");
         let card_url_para = document.createElement("p");
@@ -56,16 +80,18 @@ const create_section = (array) => {
 
         // Appends all element to their parents and finally appends them to the info_section
         card.appendChild(card_header);
-        card.appendChild(card_address_para);
-        card.appendChild(card_phone_para);
+        card.appendChild(info_div);
+        info_div.appendChild(card_address_para);
+        info_div.appendChild(card_phone_para);
         card_url_link_span.appendChild(card_url_link);
         card_url_para.appendChild(card_url_link_span);
-        card.appendChild(card_url_para);
-        card.appendChild(card_membership_level_para);
+        info_div.appendChild(card_url_para);
+        info_div.appendChild(card_membership_level_para);
         card.appendChild(card_img);
         info_section.appendChild(card);
     });
 
 }
+
 
 get_data();
