58f2ba63f080e6052389220ae131b72d71fa3b94
BoTBry <abchigbu@gmail.com>
2025-07-22 18:11:45 +0100

    week02

--- chamber/scripts/chamber(2).js
+++ chamber/scripts/chamber(2).js
@@ -1,3 +1,4 @@
+const info_section = document.querySelector(".info_section");
 const current_year = document.querySelector(".current_year");
 const lastModification = document.querySelector(".lastModification");
 const myNameElement = document.querySelector(".name");
@@ -16,30 +17,14 @@ const nav = document.querySelector("nav");
 const header = document.querySelector("header");
 const mode = document.querySelector(".mode");
 const light_dark_mode = document.querySelector("body");
+const logo = document.querySelector(".logo");
 
-const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
-const display = document.querySelector("article");
-
-// Switch grid format
-gridbutton.addEventListener("click", () => {
-	display.classList.add("grid");
-	display.classList.remove("list");
-});
-
-listbutton.addEventListener("click", function showList() {
-	display.classList.add("list");
-	display.classList.remove("grid");
-});
-
-// Hamburger Menu
 menu.addEventListener("click", () => {
     nav.classList.toggle("show");
     header.classList.toggle("close");
 
 })
 
-// Light & Dark mode btn
 mode.addEventListener("click", () => {
     mode.classList.toggle("switch");
     light_dark_mode.classList.toggle("light_background");
@@ -67,37 +52,43 @@ const create_section = (array) => {
     // Loops through the items in the array.
     array.forEach(element => {
         // Creates elements need for the info card
-        const article_grid = document.querySelector(".grid");
         let card = document.createElement("section");
-        let card_header = document.createElement("h3");
+        let info_div = document.createElement("div");
+        let card_header = document.createElement("h2");
         let card_email_para = document.createElement("p");
+        let card_url_para = document.createElement("p");
         let card_phone_para = document.createElement("p");
         let card_membership_level_para = document.createElement("p");
         let card_address_para = document.createElement("p");
+        let card_url_link_span = document.createElement("span");
         let card_url_link = document.createElement("a");
         let card_img = document.createElement("img");
 
         // Input information into created elements.
         card_header.textContent = `${element.names}`;
-        card_address_para.innerHTML = `${element.addresses}`;
-        card_email_para.innerHTML = `${element.email}`;
-        card_phone_para.innerHTML = `${element.phone_numbers}`;
+        card_address_para.innerHTML = `<strong>Address: </strong>${element.addresses}`;
+        card_email_para.innerHTML = `<strong>Email: </strong>${element.email}`;
+        card_phone_para.innerHTML = `<strong>Phone: </strong>${element.phone_numbers}`;
         card_url_link.innerHTML = `${element.website_URLs}`;
-        card_url_link.setAttribute("href", element.website_URLs);
-        card_membership_level_para.innerHTML = `${element.membership_level}`;
+        card_url_link.setAttribute("href", element.website_URLs)
+        card_url_para.innerHTML = `<strong>URL: </strong> `;
+        card_membership_level_para.innerHTML = `<strong>membership Level: </strong>${element.membership_level}`;
         card_img.setAttribute("src", element.image);
         card_img.setAttribute("alt", `${element.name} Picture`);
         card_img.setAttribute("loading", "lazy");
 
 
         // Appends all element to their parents and finally appends them to the info_section
-        card.appendChild(card_img);
         card.appendChild(card_header);
-        card.appendChild(card_address_para);
-        card.appendChild(card_phone_para);
-        card.appendChild(card_email_para);
-        card.appendChild(card_url_link);
-        article_grid.appendChild(card);
+        card.appendChild(info_div);
+        info_div.appendChild(card_address_para);
+        info_div.appendChild(card_phone_para);
+        card_url_link_span.appendChild(card_url_link);
+        card_url_para.appendChild(card_url_link_span);
+        info_div.appendChild(card_url_para);
+        info_div.appendChild(card_membership_level_para);
+        card.appendChild(card_img);
+        info_section.appendChild(card);
     });
 
 }
